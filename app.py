45b31804b82ad00c45cd4f9446c287e1e119aa24
Peijian Huang <pj@mac.mynetworksettings.com>
2025-12-08 20:16:24 -0500

    Implemented locator and about me section in the intro tab with cyberpunk style

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template  # 1. Import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, this is my SWE growth blog!"
+    # 2. Render the HTML template instead of returning a string
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
